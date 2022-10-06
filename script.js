c69478a446a80c9a48b176176b7549b27c31ddb1
JCWin <jcm91186@gmail.com>
2022-10-06 10:58:54 -0700

    Added input range slider function to change grid numbers

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// hello
 const gridSize = 512;
 const cellDefaultSize = 16;
 const gridContainer = document.querySelector('.gridcontainer');
-
-
-
+const slideValue = document.querySelector('#slidertext');
+const inputSlider = document.querySelector('input');
 
 function fillGrid(gridNum) {
     gridArea = gridNum ** 2;
@@ -15,9 +13,29 @@ function fillGrid(gridNum) {
         let cell = document.createElement('div');
         cell.setAttribute('name', `'cell${i}'`);
         cell.style.cssText = `width: ${cellDim}px; height: ${cellDim}px; 
-        box-shadow: inset 0px 0px 0px .5px #000;`;
+        box-shadow: inset 0px 0px 0px 0.5px #000;`;
         gridContainer.appendChild(cell);
     }
 }
 
-fillGrid(16);
+function clearGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+inputSlider.oninput = (() => {
+    let value = inputSlider.value;
+    let n = cellDefaultSize;
+    if (value == 3) {
+        n = 64;
+    } else if (value == 2) {
+        n = 32;
+    } else {
+        n = 16;
+    }
+    slideValue.textContent = `${n}x${n}`;
+    clearGrid();
+    fillGrid(n);
+});
+
