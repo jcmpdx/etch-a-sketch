751309acf9c4222d28293d9be35e05a1d7eb61d2
JCWin <jcm91186@gmail.com>
2022-10-08 10:14:01 -0700

    Added currColor list and started working on rainbox function

--- script.js
+++ script.js
@@ -5,23 +5,33 @@ const slideValue = document.querySelector('#slidertext');
 const inputSlider = document.querySelector('#inputslider');
 const resetButton = document.querySelector('#resetBtn');
 const colorPicker = document.querySelector('#colorpicker');
+let currColor = [];
 
+// need to make color be able to change when cells already colored with a different color. idea is to create new function to target the gridcontainers children to colorize background. take out of sizeGrid() for loop
 function sizeGrid(gridNum, color) {
     gridArea = gridNum ** 2;
-    let cellDim = gridNum * (gridSize / gridArea);
     color = colorPicker.value;
+    let cellDim = gridNum * (gridSize / gridArea);
     console.log(`${gridNum}x${gridNum}`); // remove later
     for (let i = 1; i <= gridArea; i++) {
         let cell = document.createElement('div');
         cell.setAttribute('id', `'cell${i}'`);
         cell.setAttribute('class', 'cell');
-        cell.style.cssText = `width: ${cellDim}px; height: ${cellDim}px; 
-        box-shadow: inset 0px 0px 0px .5px #000;`;
+        cell.style.cssText = `width: ${cellDim}px; height: ${cellDim}px; box-shadow: inset 0px 0px 0px .5px rgb(0,0,0,0.25);`;
         cell.addEventListener('mouseover', function() {
             cell.style.backgroundColor = `${color}`;
         })
         gridContainer.appendChild(cell);
     }
+}
+
+function rainbow() {
+    let cell = gridContainer.children;
+    cell.each(function() {
+        let i = ['blue', 'red', 'green', 'yellow'];
+        let randColor = i[Math.floor(Math.random() * i.length)];
+        cell.css('color', randColor);
+    });    
 }
 
 function clearGrid() {
@@ -50,11 +60,3 @@ resetButton.addEventListener('click', (() => { resetGrid(); }));
 
 // add 2nd argument to take color
 sizeGrid(cellDefaultSize);
-
-
-// Erase
-// document.getElementById("rainbowbtn").addEventListener("mouseout", function() {
-//     document.getElementById("rainbowbtn").style.backgroundColor = "green";
-// });
-
-// color button: 
