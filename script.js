cbe5429cbac9a124003c5f6157d40a91c1621aea
JCWin <jcm91186@gmail.com>
2022-10-08 21:46:45 -0700

    Updated init() to handle colorPicker.value

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const slideValue = document.querySelector('#slidertext');
 const inputSlider = document.querySelector('#inputslider');
 const resetButton = document.querySelector('#resetBtn');
 const colorPicker = document.querySelector('#colorpicker');
-let currColor = [];
+const randomPicker = document.querySelector('#rainbowbtn');
+let color = colorPicker.value;
+let inputVal = 0;
 
-// need to make color be able to change when cells already colored with a different color. idea is to create new function to target the gridcontainers children to colorize background. take out of sizeGrid() for loop
-function sizeGrid(gridNum, color) {
+function init(gridNum, color) {
     gridArea = gridNum ** 2;
-    color = colorPicker.value;
     let cellDim = gridNum * (gridSize / gridArea);
     console.log(`${gridNum}x${gridNum}`); // remove later
     for (let i = 1; i <= gridArea; i++) {
+        colorPicker.addEventListener('click', () => { color = colorPicker.value;
+            return color; });
+        // if randomPicker click event fire, run makeRandomColor()
         let cell = document.createElement('div');
         cell.setAttribute('id', `'cell${i}'`);
         cell.setAttribute('class', 'cell');
@@ -25,13 +28,12 @@ function sizeGrid(gridNum, color) {
     }
 }
 
-function rainbow() {
-    let cell = gridContainer.children;
-    cell.each(function() {
-        let i = ['blue', 'red', 'green', 'yellow'];
-        let randColor = i[Math.floor(Math.random() * i.length)];
-        cell.css('color', randColor);
-    });    
+function makeRandomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    color = `rgb(${r}, ${g}, ${b})`
+    return color; 
 }
 
 function clearGrid() {
@@ -41,22 +43,29 @@ function clearGrid() {
 }
 
 function resetGrid() {
-    let value = inputSlider.value;
-    let n = cellDefaultSize;
-    if (value == 3) {
-        n = 64;
-    } else if (value == 2) {
-        n = 32;
+    let inputVal = inputSlider.value;
+    let result;
+    if (inputVal == 3) {
+        result = 64;
+    } else if (inputVal == 2) {
+        result = 32;
     } else {
-        n = 16;
+        result = 16;
     }
-    slideValue.textContent = `${n}x${n}`;
+    slideValue.textContent = `${result}x${result}`;
     clearGrid();
-    sizeGrid(n);
+    init(result, color);
 }
 
+
+
+// active listeners
 inputSlider.addEventListener('input', (() => { resetGrid(); }));
 resetButton.addEventListener('click', (() => { resetGrid(); }));
+randomPicker.addEventListener('click', (() => { console.log('clicked') }));
 
 // add 2nd argument to take color
-sizeGrid(cellDefaultSize);
+init(cellDefaultSize, color);
+
+// window.onload to run init()?
+// need to make color be able to change when cells already colored with a different color. idea is to create new function to target the gridcontainers children to colorize background. take out of sizeGrid() for loop
